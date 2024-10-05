bcb8f82f64f76e9e4efb68f5d3ef2cb4caf7fafe
Shane Wade <shanebramble96@hotmail.com>
2024-10-04 19:02:51 -0500

    caputed input from command line

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello Node!");
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const printProfileData = (profileDataArr) => {
+  for (let i = 0; i < profileDataArr.length; i++) {
+    console.log(profileDataArr[i]);
+  }
+  console.log("========================");
+
+  profileDataArr.forEach((profileItem) => console.log(profileItem));
+};
+printProfileData(profileDataArgs);
