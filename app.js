6943230ead47581a379af931f25c914e74538153
Shane Wade <shanebramble96@hotmail.com>
2024-10-08 18:10:26 -0500

    installed inquirer npm module

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-const fs = require("fs");
-const generatePage = require('./src/page-template');
+import inquirer from "inquirer"; // CommonJS syntax
+// const fs = require("fs");
+// const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name,github);
 
-const [name, github] = profileDataArgs;
+// fs.writeFile("./index.html", pageHTML, (err) => {
+//   // If error exists throw an error.
+//   if (err) throw err;
 
-fs.writeFile("./index.html", generatePage(name, github), (err) => {
-  // If error exists throw an error.
-  if (err) throw err;
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
 
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+  ])
+  .then((answers) => console.log(answers));
