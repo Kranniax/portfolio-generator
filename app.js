d6b3679b2819a2e52100d09026b41c7cec63bf42
Shane Wade <shanebramble96@hotmail.com>
2024-10-09 18:49:31 -0500

    made some comments

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 import inquirer from "inquirer"; // Inport NPM module.
-// const fs = require("fs");
-// const generatePage = require('./src/page-template');
+const fs = require("fs");
+const generatePage = require('./src/page-template.js');
 
-// const pageHTML = generatePage(name,github);
-
-// fs.writeFile("./index.html", pageHTML, (err) => {
-//   // If error exists throw an error.
-//   if (err) throw err;
-
-//   console.log("Portfolio complete! Check out index.html to see the output!");
-// });
 // A prompt asking users about themselves.
 const promptUser = () => {
   return inquirer.prompt([
@@ -154,5 +146,12 @@ const promptProject = (portfolioData) => {
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    // fs.writeFile("./index.html", pageHTML, (err) => {
+    //   // If error exists throw an error.
+    //   if (err) throw err;
+
+    //   console.log("Portfolio complete! Check out index.html to see the output!");
+    // });
   });
